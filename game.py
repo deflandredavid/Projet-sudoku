645d8139ea45a8d1045a53efd12f2e5a5715a925
David Deflanfre <david.deflandre45@gmail.com>
2024-10-08 20:52:58 +0200

    Initial commit

---
+++ game.py
@@ -0,0 +1,17 @@
+import pygame
+
+WIDTH = 550
+background_color = (251,247,245)
+
+def main():
+    pygame.init()
+    win = pygame.display.set_mode((WIDTH,WIDTH))
+    pygame.display.set_caption("Sudoku")
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+            
+main()
